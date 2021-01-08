edb0f5362cd00c4536640bf5969e2670ed0d9f41
parva2 <32992177+parva2@users.noreply.github.com>
2021-01-08 15:37:08 -0600

    Update config.js

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "US East (Ohio) us-east-2	",
+      BUCKET: "brightmed-loginbucket",
+    },
+    apiGateway: {
+      REGION: "YOUR_API_GATEWAY_REGION",
+      URL: "YOUR_API_GATEWAY_URL",
+    },
+    cognito: {
+      REGION: "YOUR_COGNITO_REGION",
+      USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
+      APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
+      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID",
+    },
+  };
+  
+  export default config;
